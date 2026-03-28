510c9764c943836bf6ce2bc25389fbe6a7483c29
Rhafi Aulia Rahman <rhafiauliarahman818181@gmail.com>
2026-03-29 01:42:00 +0700

    Revert "add react-router and axios"
    This reverts commit dd591b411ea89705208f522e4c3c1028d2f4b321.

--- main.tsx
+++ main.tsx
@@ -1,17 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './src/App.tsx'
-import React from "react";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router}/>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
