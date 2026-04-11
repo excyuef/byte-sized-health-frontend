d26e93cdca1d6263581dc57957ae616cfd76a97e
Rhafi Aulia Rahman <rhafiauliarahman818181@gmail.com>
2026-04-11 20:08:04 +0700

    2

--- src/views/components/MainCardLogin.tsx
+++ src/views/components/MainCardLogin.tsx
@@ -1,4 +1,3 @@
-import Logo from "../../assets/logo.jpeg";
 import SecondCardLogin from "./SecondCardLogin";
 import HomeIcon from "./HomeIcon"
 
@@ -7,7 +6,6 @@ function MainCardLogin() {
     <div className="bg-white w-220 h-140 rounded-2xl flex items-center justify-center shadow-2xl p-4">
       <div className="flex flex-col items-center justify-between gap-45 px-5 w-1/2">
         <div className="flex flex-col items-center justify-center gap-6">
-          <img src={Logo} alt="Logo" className="w-54 h-auto" />
           <h2 className="font-bold text-4xl">Welcome Back!</h2>
         </div>
         <div className="flex flex-col items-center justify-center">
@@ -19,7 +17,7 @@ function MainCardLogin() {
           </p>
           <p className="font-bold">
             <a href="#"
-            className="flex gap-2 items-center">Kembali ke Beranda <HomeIcon /></a>
+              className="flex gap-2 items-center">Kembali ke Beranda <HomeIcon /></a>
           </p>
         </div>
       </div>
