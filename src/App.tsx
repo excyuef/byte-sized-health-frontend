f1502b98280f72cb826132e233a3bcee4d3f49e5
Rhafi Aulia Rahman <rhafiauliarahman818181@gmail.com>
2026-04-12 14:42:22 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import "./App.css";
-import ButtonCheckIn from "./views/components/ButtonCheckIn.tsx";
 import CheckIn from "./views/components/Checln.tsx";
-import IconCheckIn from "./views/components/IconCheckIn.tsx";
-import InputCheckIn from "./views/components/InputCheckIn.tsx";
-// import Nav from "./views/components/NavBar.tsx";
-// import Hero from "./views/components/Hero.tsx";
-// import Intro from "./views/components/Intro.tsx";
-import Login from "./views/components/Login.tsx";
-import MainCardRegister from "./views/components/MainCardRegister.tsx";
 
 function App() {
   return (
-    <div className="flex items-center justify-center mt-22">
+    <div className="flex items-center justify-center my-22">
       <CheckIn />
     </div>
   );
--- src/views/components/MainCardRegister.tsx
+++ src/views/components/MainCardRegister.tsx
@@ -1,14 +1,10 @@
-import Logo from "../../assets/Logo.jpeg";
-import TahapMasukkanAkun from "./TahapMasukkanAkun";
 import TahapIsiDataDiri from "./TahapIsiDataDiri";
-import TahapSelesai from "./TahapSelesai";
 
 function MainCardRegister() {
   return (
     <div className="bg-white w-220 h-140 rounded-2xl flex items-center justify-center shadow-2xl p-4">
       <div className="flex flex-col items-center justify-between gap-45 px-5 w-1/2">
         <div className="flex flex-col items-center justify-center gap-6">
-          <img src={Logo} alt="Logo" className="w-54 h-auto" />
           <h2 className="font-bold text-4xl">Hello!</h2>
         </div>
         <div className="flex flex-col items-center justify-center">
@@ -23,7 +19,7 @@ function MainCardRegister() {
           </p>
         </div>
       </div>
-      <TahapIsiDataDiri/>
+      <TahapIsiDataDiri />
     </div>
   );
 }
